40e89955e64762ff11ac3ba4817ef94910b5cb0d
argonsys <kenon12552@icloud.com>
2023-11-28 21:08:03 +0900

    正規表現によるバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  REGFORMAT_KATAKANA = /^[ア-ヶー]+$/
+  REGFORMAT_JAPANESE = /^[ぁ-んァ-ヶ一-龥々]+$/
+  REGFORMAT_ALPHANUM = /^(?=.*?[a-z])(?=.*?\d)[a-z\d]+$/i
+  MESSAGE_INVALID_KATAKANA = 'にはカタカナを入力してください。'.freeze
+  MESSAGE_INVALID_JAPANESE = 'には漢字・かな・カタカナが使えます。'.freeze
+  MESSAGE_INVALID_ALPHANUM = 'は英数字混合で入力してください。'.freeze
+
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :password, format: { with: REGFORMAT_ALPHANUM, message: MESSAGE_INVALID_ALPHANUM }
+  validates :first_name, presence: true, format: { with: REGFORMAT_JAPANESE, message: MESSAGE_INVALID_JAPANESE }
+  validates :last_name, presence: true, format: { with: REGFORMAT_JAPANESE, message: MESSAGE_INVALID_JAPANESE }
+  validates :first_name_kana, presence: true, format: { with: REGFORMAT_KATAKANA, message: MESSAGE_INVALID_KATAKANA }
+  validates :last_name_kana, presence: true, format: { with: REGFORMAT_KATAKANA, message: MESSAGE_INVALID_KATAKANA }
   validates :birthday, presence: true
 end
