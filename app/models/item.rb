970c4ecd6217249791a9f905e04d19c8e86ce2b6
argonsys <kenon12552@icloud.com>
2023-11-29 18:10:11 +0900

    itemモデルにvalidationの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true, numerality: { other_than: 1 }
+  validates :condition_id, presence: true, numerality: { other_than: 1 }
+  validates :shipping_cost_on, presence: true, numerality: { other_than: 1 }
+  validates :prefecture_id, presence: true, numerality: { other_than: 1 }
+  validates :days_to_shipping_id, presence: true, numerality: { other_than: 1 }
+  validates :price, presence: true, numerality: true
 end
