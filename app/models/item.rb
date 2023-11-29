6e420f2a2c4ab68106dadcd33a25f3eb6289a097
argonsys <kenon12552@icloud.com>
2023-11-29 19:29:47 +0900

    numericalityのタイプミスを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true, numerality: { other_than: 1 }
-  validates :condition_id, presence: true, numerality: { other_than: 1 }
-  validates :shipping_cost_on, presence: true, numerality: { other_than: 1 }
-  validates :prefecture_id, presence: true, numerality: { other_than: 1 }
-  validates :days_to_shipping_id, presence: true, numerality: { other_than: 1 }
-  validates :price, presence: true, numerality: true
+  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :condition_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_cost_on, presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :days_to_shipping_id, presence: true, numericality: { other_than: 1 }
+  validates :price, presence: true, numericality: true
 
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtentions
