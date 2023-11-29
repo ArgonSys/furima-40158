23e7e9d702fb587a6e8e2283dba9e8c892478b0b
argonsys <kenon12552@icloud.com>
2023-11-29 19:54:01 +0900

    items#createアクションとストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_cost_on_id,
+                                 :prefecture_id, :days_to_shipping_id, :price).merge(user_id: current_user.id)
   end
 end
