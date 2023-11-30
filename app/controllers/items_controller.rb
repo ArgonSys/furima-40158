200cfba105c9fdff3457586ec3a8cf2111332a13
argonsys <kenon12552@icloud.com>
2023-11-30 21:44:09 +0900

    出品者でないユーザーに対するのリダイレクト機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_no_sellers_to_show, only: [:edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
     @have_no_item = Item.none?
@@ -42,5 +43,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def redirect_no_sellers_to_show
+    redirect_to :show if current_user != @item.user_id
   end
 end
