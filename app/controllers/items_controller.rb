6062299809f600342272b0600abcf0d7af23d579
argonsys <kenon12552@icloud.com>
2023-11-30 22:02:41 +0900

    出品者でないユーザーの判定を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :redirect_no_sellers_to_root, only: [:edit, :update]
+
   def index
     @items = Item.all.order('created_at DESC')
     @have_no_item = Item.none?
@@ -46,6 +47,6 @@ class ItemsController < ApplicationController
   end
 
   def redirect_no_sellers_to_root
-    redirect_to root_path if current_user != @item.user_id
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
