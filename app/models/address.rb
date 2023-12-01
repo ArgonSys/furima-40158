4c25413f032c1b9c2b8e0c943c5257a9c27c7adf
argonsys <kenon12552@icloud.com>
2023-12-01 16:36:08 +0900

    PurchaseAddressフォームオブジェクトの作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,3 @@
 class Address < ApplicationRecord
-  validates :postcode, presence: true
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
-  validates :municipality, presence: true
-  validates :street_address, presence: true
-  validates :building
-  validates :phone_number, presence: true
-
   belongs_to :purchase
 end
---
+++ app/models/purchase_address.rb
@@ -0,0 +1,16 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postcode, :prefecture_id, :municipality, :street_address, :building, :phone_number, :user_id, :item_id
+
+  validates :postcode, presence: true
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :municipality, presence: true
+  validates :street_address, presence: true
+  validates :building
+  validates :phone_number, presence: true
+
+  def save
+    purchase = Purchase.create(user_id:, item_id:)
+    Address.create(postcode:, prefecture_id:, municipality:, street_address:, building:, phone_number:, purchase_id: purchase.id)
+  end
+end
