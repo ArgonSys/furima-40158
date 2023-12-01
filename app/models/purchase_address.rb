639ed8180b95ad89a07121ebf50a527f0ebfee07
argonsys <kenon12552@icloud.com>
2023-12-01 20:54:48 +0900

    postcode, phone_numberに正規表現によるバリデーションを追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,11 +2,13 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postcode, :prefecture_id, :municipality, :street_address, :building, :phone_number, :user_id, :item_id, :token
 
-  validates :postcode, presence: true
+  REGEXP_POSTCODE = /\A\d{3}-\d{4}\z/
+  REGEXP_PHONE_NUMBER = /\A\d{10,11}\z/
+  validates :postcode, presence: true, format: { with: REGEXP_POSTCODE }
   validates :prefecture_id, presence: true, numericality: { other_than: 1 }
   validates :municipality, presence: true
   validates :street_address, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, format: { with: REGEXP_PHONE_NUMBER }
   validates :user_id, presence: true, numericality: true
   validates :item_id, presence: true, numericality: true
   validates :token, presence: true
