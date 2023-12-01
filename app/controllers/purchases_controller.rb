31c7cdf6574a9dea98e8c02f9ab70dd5046b8b7b
argonsys <kenon12552@icloud.com>
2023-12-01 16:54:16 +0900

    購入画面の商品表示実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,17 @@
 class PurchasesController < ApplicationController
-  def new
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase = PurchaseAddress.new
   end
 
   def create
+    @purchase = PurchaseAddress.new(purchase_params)
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postcode, :prefecture_id, :municipality, :street_address,
+                                             :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
