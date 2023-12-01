f3faa802d7beabb63829f1b7f4adb53b067cbf57
argonsys <kenon12552@icloud.com>
2023-12-01 16:43:48 +0900

    buildingバリデーションの削除

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,7 +6,6 @@ class PurchaseAddress
   validates :prefecture_id, presence: true, numericality: { other_than: 1 }
   validates :municipality, presence: true
   validates :street_address, presence: true
-  validates :building
   validates :phone_number, presence: true
 
   def save
