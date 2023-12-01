da00865bc21ca382e6a3e4c838195cd28272a388
argonsys <kenon12552@icloud.com>
2023-12-01 15:46:57 +0900

    アソシエーションとバリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
 class Address < ApplicationRecord
+  validates :postcode, presence: true
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :municipality, presence: true
+  validates :street_address, presence: true
+  validates :building
+  validates :phone_number, presence: true
+
+  belongs_to :purchase
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,7 @@ class Item < ApplicationRecord
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
+  has_one :purchase
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_cost_on
